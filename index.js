cce79913698fd8436066282e26ca6146972aa193
Sercan Şimşek <sercansimsek44@gmail.com>
2026-05-28 14:21:41 +0200

    parcel added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const picOfTheDay = document.querySelector(".picOfTHeDay");
+const picTitle = document.querySelector(".picTitle");
+
 const apiKey = process.env.NASA_API_KEY;
 
 async function getApod() {
@@ -6,14 +8,17 @@ async function getApod() {
     const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
-    displayApod(data)
+    console.log(data);
+    displayApod(data);
   } catch (error) {
     console.error("Something went wrong!", error);
   }
 }
 
 function displayApod(data) {
+  picTitle.textContent = data.title;
   const image = document.createElement("img");
+  image.style.width = 300 + "px";
   image.src = `${data.hdurl}`;
   picOfTheDay.appendChild(image);
 }
